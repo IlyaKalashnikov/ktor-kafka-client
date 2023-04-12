d9eda1de46285c37ef7da505f1976337d6fe9a27
Ilya Kalashnikov <i.kalashnikov@oson.uz>
2023-04-12 11:17:32 +0500

    fix: default serdes configs added

--- src/main/kotlin/org/sour/cabbage/soup/Streams.kt
+++ src/main/kotlin/org/sour/cabbage/soup/Streams.kt
@@ -2,6 +2,7 @@ package org.sour.cabbage.soup
 
 import io.ktor.server.application.*
 import org.apache.kafka.common.serialization.Serde
+import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.StreamsConfig
@@ -32,12 +33,19 @@ fun <K, V> kafkaStreams(configuration: KafkaStreamsConfig<K, V>): KafkaStreams {
     return KafkaStreams(configuration.builder.build(), buildConfigs(configuration))
 }
 
+/**
+ * For key serialization/deserialization process String-serializer/deserializer would be used by default
+ * For value serialization/deserialization process ByteArray-serializer/deserializer would be used by default
+ * Both of this configs would be overridden by provided keySerde-valueSerde in KafkaStreamsConfig
+ */
 private fun <K, V> buildConfigs(configuration: KafkaStreamsConfig<K, V>): Properties { //TODO: make it more configurable
     val props = Properties()
     props[StreamsConfig.APPLICATION_ID_CONFIG] =
         configuration.context.config.propertyOrNull("ktor.kafka.application-id")?.getString()
     props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] =
         configuration.context.config.propertyOrNull("ktor.kafka.bootstrap-servers")?.getString()
+    props[StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG] = Serdes.String().javaClass
+    props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = Serdes.ByteArray().javaClass
     return props
 }
 
